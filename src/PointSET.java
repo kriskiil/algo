6dbbba2f485fc866481a0fd0115b63572030e0c8
kiil <kris.kiil@gmail.com>
2015-10-05 22:29:29 +0200

    Implemented PointSET. Still untested, but no compile errors.

--- src/PointSET.java
+++ src/PointSET.java
@@ -1,48 +1,74 @@
+import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.StdDraw;
 
 
 public class PointSET {
-	Node root = null;
+	SET<Point2D> set;
 	private class Node {
 		Node right;
 		Node left;
-		Point2D key;
-		private int size = 0;
-		boolean color;
+		Point2D p;
+		RectHV rect;
 		public Node(Point2D key) {
-			this.key = key;
-		}
-		public int size() {
-			return size;
+			this.p = key;
 		}
 	}
 	public PointSET() {
 		// construct an empty set of points
+		set = new SET<Point2D>();
 	}
 	public boolean isEmpty() {
 		   // is the set empty? 
-		return root == null;
+		return set.isEmpty();
 	}
 	public int size() {
 		   // number of points in the set
-		if (isEmpty()) return 0;
-		return root.size();
+		return set.size();
 	}
 	public void insert(Point2D p) {
 		// add the point to the set (if it is not already in the set)
-		
+		set.add(p);
 	}
 	public boolean contains(Point2D p) {
-		   // does the set contain point p? 
+		   // does the set contain point p?
+		return set.contains(p);
 	}
 	public void draw() {
-		   // draw all points to standard draw 
+		   // draw all points to standard draw
+		StdDraw.setXscale();
+		StdDraw.setYscale();
+		StdDraw.setPenRadius(0.01);
+		StdDraw.setPenColor(StdDraw.BLACK);
+		for (Point2D p: set) {
+			StdDraw.point(p.x(), p.y());
+		}
+		StdDraw.show();
 	}
 	public Iterable<Point2D> range(RectHV rect) {
-		   // all points that are inside the rectangle 
+		   // all points that are inside the rectangle
+		Bag<Point2D> result = new Bag<Point2D>();
+		for (Point2D p: set) {
+			if(rect.distanceSquaredTo(p) == 0) {
+				result.add(p);
+			}
+		}
+		return result;
 	}
 	public Point2D nearest(Point2D p) {
 		   // a nearest neighbor in the set to point p; null if the set is empty 
+		Point2D champion = null;
+		double mindist = Double.POSITIVE_INFINITY;
+		for (Point2D q: set) {
+			double dist = p.distanceSquaredTo(q);
+			if(dist < mindist) {
+				mindist = dist;
+				champion = q;
+			}
+		}
+		return champion;
 	}
 
 	/**
